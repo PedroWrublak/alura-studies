2dcd1fe0ce713ebd9d4a26aacb22b8d5c37fa9d3
Pedro Wrublak <pedrovili.w@gmail.com>
2025-03-30 13:52:32 -0300

    feat: add values for task and time | function addTask

--- src/components/List/index.tsx
+++ src/components/List/index.tsx
@@ -1,33 +1,40 @@
-import React, { useState } from 'react';
-import style from './List.module.scss';
-import Item from './Item';
+import React, { useState } from "react";
+import style from "./List.module.scss";
+import Item from "./Item";
 
 function List() {
-    const [tasks, setTasks] = useState([{ //useStat
-        task: 'React',
-        time: '02:00:00'
-    }, {
-        task: 'Javascript',
-        time: '01:00:00'
-    }, {
-        task: 'Typescript',
-        time: '0:00:00'
-    }]);
-    return (
-        <aside className={style.listaTarefas}>
-            <h2 onClick={() => {
-                setTasks([...tasks, { task: "Estudar estado", time: "05:00:00"}])
-            }}> Estudos do Dia </h2>
-            <ul>
-                {tasks.map((item, index) => (
-                   <Item
-                        key={index}
-                        {...item}
-                   />
-                ))}
-            </ul>
-        </aside>
-    )
+  const [tasks, setTasks] = useState([
+    {
+      //useStat
+      task: "React",
+      time: "02:00:00",
+    },
+    {
+      task: "Javascript",
+      time: "01:00:00",
+    },
+    {
+      task: "Typescript",
+      time: "0:00:00",
+    },
+  ]);
+  return (
+    <aside className={style.listaTarefas}>
+      <h2
+        onClick={() => {
+          setTasks([...tasks, { task: "Estudar estado", time: "05:00:00" }]);
+        }}
+      >
+        {" "}
+        Estudos do Dia{" "}
+      </h2>
+      <ul>
+        {tasks.map((item, index) => (
+          <Item key={index} {...item} />
+        ))}
+      </ul>
+    </aside>
+  );
 }
 
 export default List;
